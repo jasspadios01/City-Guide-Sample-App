a95505aa4664307879568f60f6971cd73fc456c5
Jass Padios <91588581+jasspadios01@users.noreply.github.com>
2021-11-16 14:37:08 +0800

    City Guide (Sample) App

---
+++ app/src/main/java/com/example/cityguidesample/DavaoCity.java
@@ -0,0 +1,14 @@
+package com.example.cityguidesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DavaoCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_davao_city);
+    }
+}
---
+++ app/src/main/java/com/example/cityguidesample/IloiloCity.java
@@ -0,0 +1,14 @@
+package com.example.cityguidesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IloiloCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iloilo_city);
+    }
+}
